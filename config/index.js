7d3176e34d3ca2af8ebd89710c397c20cf77cd43
zhuwenjun00 <113602810+zhuwenjun00@users.noreply.github.com>
2022-09-15 20:07:48 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wx44ef056cffac89ec",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "b2afe47cc5d9499dc24690b621b45528",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -20,24 +20,24 @@ export const config = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [
+  USERS: [oGrBI69HYrc-KRvpz0iBguaBW6,oGrBI6ywAB3iG3i4RSf4gvRQCYTs]
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "李玲玉",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oGrBI69HYrc-KRvpz0iBguaBW66I",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "g9xNzfhR7ofIA3UJepqCC8i9rPwrl1Cq_I-WZBlCUeo",
       // 所在省份
       province: "广东",
       // 所在城市
-      city: "肇庆",
+      city: "广州",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '05-15',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "https://",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
